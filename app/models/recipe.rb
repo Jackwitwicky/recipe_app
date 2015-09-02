491e23b5e8a5840b913ae0f9380368b2018f8446
Jackwitwicky <jacknkiarie@gmail.com>
2015-09-02 12:06:04 +0300

    Done with ingredients and directions

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,12 @@
 class Recipe < ActiveRecord::Base
+  has_many :directions
+  has_many :ingredients
+
+  accepts_nested_attributes_for :ingredients, reject_if: :all_blank,
+                                allow_destroy: true
+  accepts_nested_attributes_for :directions, reject_if: :all_blank,
+                                allow_destroy: true
+
   has_attached_file :image, styles: { medium: "300x300#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
