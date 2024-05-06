3fec3110669aedc895a1334cb30db81ad8e1e6f1
Jack Kiarie <jacknkiarie@gmail.com>
2024-05-06 13:27:27 +0300

    run rails app update task

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  root "recipes#index"
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 
-  get 'login'     =>  "sessions#new"
-  post 'login'    =>  "sessions#create"
-  delete 'logout' =>  "sessions#destroy"
+  # You can have the root of your site routed with "root"
+  root 'recipes#index'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   get 'about' => "recipes#about"
 
